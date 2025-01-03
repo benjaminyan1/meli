49499b5f53e2a1391cf31e3817e65e94be9c4101
Benjamin <by58@duke.edu>
2025-01-03 00:00:43 -0500

    users fix

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -6,7 +6,7 @@ from .models import Review
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ["id", "first_name", "last_name", "email", "username", "password", ]
+        fields = ["id", "first_name", "last_name", "email", "username", "password"]
         extra_kwargs = {"password": {"write_only": True}}
 
     def create(self, validated_data):
